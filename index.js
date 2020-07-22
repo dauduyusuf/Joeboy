752dacf557ec6f640fa2e3240ec10bff370cdaf6
VictorDaudu <Daudu.victor01@gmail.com>
2020-07-22 01:24:14 +0100

    created gallery and added AOS effect

--- index.js
+++ index.js
@@ -4,20 +4,63 @@
 AOS.init({
 	easing: 'ease-in-out-sine',
 });
-// Instastory
 
+// Gallery
+$('#nanogallery2').nanogallery2({
+	items: [
+		{
+			src: './src/images/joeboy5.jpg',
+			srct: './src/images/joeboy5.jpg',
+		},
+		{
+			src: './src/images/Joeboy Hints.jpg',
+			srct: './src/images/Joeboy Hints.jpg',
+		},
+		{
+			src: './src/images/Joeboy Releases.jpg',
+			srct: './src/images/Joeboy Releases.jpg',
+		},
+		{
+			src: './src/images/joeboyofficial.jpg',
+			srct: './src/images/joeboyofficial.jpg',
+		},
+		{
+			src: './src/images/joebogrid.jpg',
+			srct: './src/images/joebogrid.jpg',
+		},
+		{
+			src: './src/images/joeboy4.jpg',
+			srct: './src/images/joeboy4.jpg',
+		},
+	],
+	thumbnailWidth: 400,
+	thumbnailHeight: 320,
+});
 
 // FUNCTIONS
 function openMenu() {
-	$('#mobilemenu').slideToggle(800);
-	$('#mobilemenu').removeClass('hide');
+	$('#sideBar').fadeToggle();
 	$('#toggleBtn').toggleClass('close');
+	if (!$('#toggleBtn').hasClass('close')) {
+		$('#intro .social-menu')
+			.css('right', 'unset')
+			.animate({ left: '20px' }, 'slow');
+	} else {
+		$('#intro .social-menu')
+			.animate({ right: '30px' }, 'slow')
+			.css('left', 'unset');
+	}
 }
-
+$('#song > div').on('click', function () {
+	if ($('.songlinks').hasClass('fadeInUp')) {
+		$('.songlinks').addClass('fadeOutDown');
+	}
+	$(this).find('.songlinks').toggleClass('fadeOutDown fadeInUp');
+});
 // EVENTS
 $(document).ready(function () {
 	$(window).scroll(function () {
-		if ($(window).scrollTop() > 2000) {
+		if ($(window).scrollTop() > 750) {
 			$('#backtoTop').addClass('animated zoomInUp');
 			$('#backtoTop').css('display', 'grid');
 			$('#backtoTop').removeClass('zoomOutDown');
